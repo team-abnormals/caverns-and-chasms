6cdecef957a806a3f153d96d0e89ddaea2ceda94
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-10 15:57:33 -0600

    Deeper explosion changes
    * Deepers now create fire if they are on fire
    * Deepers now break blocks rather than destroy them

--- src/main/java/com/minecraftabnormals/caverns_and_chasms/common/entity/DeeperEntity.java
+++ src/main/java/com/minecraftabnormals/caverns_and_chasms/common/entity/DeeperEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
 public class DeeperEntity extends CreeperEntity {
@@ -16,5 +17,17 @@ public class DeeperEntity extends CreeperEntity {
 	@Override
 	public ItemStack getPickedResult(RayTraceResult target) {
 		return new ItemStack(CCItems.DEEPER_SPAWN_EGG.get());
+	}
+
+	@Override
+	public void explode() {
+		if (!this.world.isRemote) {
+			Explosion.Mode explosion$mode = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.world, this) ? Explosion.Mode.BREAK : Explosion.Mode.NONE;
+			float f = this.isCharged() ? 2.0F : 1.0F;
+			this.dead = true;
+			this.world.createExplosion(this, this.getPosX(), this.getPosY(), this.getPosZ(), (float) this.explosionRadius * f, this.isBurning(), explosion$mode);
+			this.remove();
+			this.spawnLingeringCloud();
+		}
 	}
 }
