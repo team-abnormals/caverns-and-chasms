dca069a7c5fb033a6b47c3b8b53aae81f2b243b6
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-03-14 23:10:21 -0500

    WIP particles

--- src/main/java/com/minecraftabnormals/caverns_and_chasms/core/registry/CCParticles.java
+++ src/main/java/com/minecraftabnormals/caverns_and_chasms/core/registry/CCParticles.java
@@ -2,8 +2,7 @@ package com.minecraftabnormals.caverns_and_chasms.core.registry;
 
 import com.minecraftabnormals.caverns_and_chasms.core.CavernsAndChasms;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.particle.FlameParticle;
-import net.minecraft.client.particle.LavaParticle;
+import net.minecraft.client.particle.*;
 import net.minecraft.particles.BasicParticleType;
 import net.minecraft.particles.ParticleType;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
@@ -19,6 +18,8 @@ public class CCParticles {
 
 	public static final RegistryObject<BasicParticleType> CURSED_FLAME = createBasicParticleType(true, "cursed_flame");
 	public static final RegistryObject<BasicParticleType> CURSED_AMBIENT = createBasicParticleType(true, "cursed_ambient");
+	public static final RegistryObject<BasicParticleType> MIME_ENERGY = createBasicParticleType(true, "mime_energy");
+	public static final RegistryObject<BasicParticleType> MIME_SPARK = createBasicParticleType(true, "mime_spark");
 
 	private static RegistryObject<BasicParticleType> createBasicParticleType(boolean alwaysShow, String name) {
 		return PARTICLES.register(name, () -> new BasicParticleType(alwaysShow));
@@ -31,6 +32,8 @@ public class CCParticles {
 		public static void registerParticleTypes(ParticleFactoryRegisterEvent event) {
 			Minecraft.getInstance().particles.registerFactory(CURSED_FLAME.get(), FlameParticle.Factory::new);
 			Minecraft.getInstance().particles.registerFactory(CURSED_AMBIENT.get(), LavaParticle.Factory::new);
+			Minecraft.getInstance().particles.registerFactory(MIME_ENERGY.get(), CloudParticle.Factory::new);
+			Minecraft.getInstance().particles.registerFactory(MIME_SPARK.get(), CloudParticle.Factory::new);
 		}
 	}
 }
