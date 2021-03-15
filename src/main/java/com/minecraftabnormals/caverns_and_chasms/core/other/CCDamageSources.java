84bf1a97cca120314ec091eab6ca0ab75e0cfede
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-03-14 21:57:12 -0500

    Fixed custom DamageSources

--- src/main/java/com/minecraftabnormals/caverns_and_chasms/core/other/CCDamageSources.java
+++ src/main/java/com/minecraftabnormals/caverns_and_chasms/core/other/CCDamageSources.java
@@ -1,8 +1,18 @@
 package com.minecraftabnormals.caverns_and_chasms.core.other;
 
+import com.minecraftabnormals.caverns_and_chasms.common.entity.KunaiEntity;
+import com.minecraftabnormals.caverns_and_chasms.core.CavernsAndChasms;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.IndirectEntityDamageSource;
+
+import javax.annotation.Nullable;
 
 public class CCDamageSources {
-	public static final DamageSource AFFLICTION = new DamageSource("affliction").setDamageBypassesArmor().setMagicDamage();
-	public static final DamageSource SPIKED_RAIL = new DamageSource("spiked_rail");
+	public static final DamageSource AFFLICTION = new DamageSource(CavernsAndChasms.MOD_ID + ".affliction").setDamageBypassesArmor().setMagicDamage();
+	public static final DamageSource SPIKED_RAIL = new DamageSource(CavernsAndChasms.MOD_ID + ".spiked_rail");
+
+	public static DamageSource causeKunaiDamage(KunaiEntity kunai, @Nullable Entity indirectEntity) {
+		return (new IndirectEntityDamageSource(CavernsAndChasms.MOD_ID + ".kunai", kunai, indirectEntity)).setProjectile();
+	}
 }
