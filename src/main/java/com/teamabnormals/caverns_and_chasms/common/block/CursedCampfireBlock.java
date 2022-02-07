f80dfa7f921322b177b4e7b0ed04521c117eeccd
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-02-07 10:53:46 -0600

    Fixed Cursed Campfire not ticking

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/block/CursedCampfireBlock.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/block/CursedCampfireBlock.java
@@ -1,6 +1,7 @@
 package com.teamabnormals.caverns_and_chasms.common.block;
 
 import com.teamabnormals.caverns_and_chasms.common.block.entity.CursedCampfireBlockEntity;
+import com.teamabnormals.caverns_and_chasms.core.registry.CCBlockEntityTypes;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCParticleTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
@@ -12,8 +13,12 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.CampfireBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 public class CursedCampfireBlock extends CampfireBlock {
@@ -46,6 +51,16 @@ public class CursedCampfireBlock extends CampfireBlock {
 	public void entityInside(BlockState state, Level worldIn, BlockPos pos, Entity entityIn) {
 		if (!entityIn.fireImmune() && state.getValue(LIT) && entityIn instanceof LivingEntity && ((LivingEntity) entityIn).isInvertedHealAndHarm() && !EnchantmentHelper.hasFrostWalker((LivingEntity) entityIn)) {
 			entityIn.hurt(DamageSource.IN_FIRE, 4.0F);
+		}
+	}
+
+	@Nullable
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+		if (level.isClientSide) {
+			return state.getValue(LIT) ? createTickerHelper(blockEntityType, CCBlockEntityTypes.CURSED_CAMPFIRE.get(), CampfireBlockEntity::particleTick) : null;
+		} else {
+			return state.getValue(LIT) ? createTickerHelper(blockEntityType, CCBlockEntityTypes.CURSED_CAMPFIRE.get(), CampfireBlockEntity::cookTick) : createTickerHelper(blockEntityType, CCBlockEntityTypes.CURSED_CAMPFIRE.get(), CampfireBlockEntity::cooldownTick);
 		}
 	}
 }
