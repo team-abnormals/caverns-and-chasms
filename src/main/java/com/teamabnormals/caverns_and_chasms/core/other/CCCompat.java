9aca63b14843cdf2e882f63881430ce4d509c0cf
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-16 22:24:59 -0500

    Remove unneeded BiomeDictionary additions

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCCompat.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCCompat.java
@@ -17,33 +17,17 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DispenserBlock;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
-
-import static net.minecraftforge.common.BiomeDictionary.Type.*;
 
 public class CCCompat {
 
 	public static void registerCompat() {
-		addBiomeDictionaryTypes();
 		registerDispenserBehaviors();
 		changeLocalization();
 		setFireproof();
 		addWaxables();
-	}
-
-	private static void addBiomeDictionaryTypes() {
-		BiomeDictionary.addTypes(Biomes.MEADOW, PLAINS, MOUNTAIN, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.GROVE, COLD, CONIFEROUS, FOREST, SNOWY, MOUNTAIN, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.SNOWY_SLOPES, COLD, SPARSE, SNOWY, MOUNTAIN, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.JAGGED_PEAKS, COLD, SPARSE, SNOWY, MOUNTAIN, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.FROZEN_PEAKS, COLD, SPARSE, SNOWY, MOUNTAIN, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.STONY_PEAKS, HOT, MOUNTAIN, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.LUSH_CAVES, LUSH, WET, OVERWORLD);
-		BiomeDictionary.addTypes(Biomes.DRIPSTONE_CAVES, SPARSE, OVERWORLD);
 	}
 
 	private static void registerDispenserBehaviors() {
