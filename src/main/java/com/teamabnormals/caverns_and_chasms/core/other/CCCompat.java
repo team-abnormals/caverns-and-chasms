c7ad98e7ca3513326376f64c100bad9ca1c62e99
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-30 15:37:14 -0500

    Made Azalea Wood flammable

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCCompat.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCCompat.java
@@ -4,7 +4,7 @@ import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableBiMap;
 import com.teamabnormals.blueprint.common.dispenser.FishBucketDispenseItemBehavior;
 import com.teamabnormals.blueprint.core.util.DataUtil;
-import com.teamabnormals.caverns_and_chasms.common.entity.projectile.SilverArrow;
+import com.teamabnormals.caverns_and_chasms.common.entity.projectile.Kunai;
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCBlocks;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
@@ -24,17 +24,37 @@ import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 public class CCCompat {
 
 	public static void registerCompat() {
+		registerFlammables();
 		registerDispenserBehaviors();
 		changeLocalization();
 		setFireproof();
 		addWaxables();
 	}
 
+	private static void registerFlammables() {
+		DataUtil.registerFlammable(CCBlocks.AZALEA_LOG.get(), 5, 5);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_WOOD.get(), 5, 5);
+		DataUtil.registerFlammable(CCBlocks.STRIPPED_AZALEA_LOG.get(), 5, 5);
+		DataUtil.registerFlammable(CCBlocks.STRIPPED_AZALEA_WOOD.get(), 5, 5);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_PLANKS.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_SLAB.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_STAIRS.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_FENCE.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_FENCE_GATE.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_BOARDS.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_VERTICAL_SLAB.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_BOOKSHELF.get(), 30, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_BEEHIVE.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_POST.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.STRIPPED_AZALEA_POST.get(), 5, 20);
+		DataUtil.registerFlammable(CCBlocks.AZALEA_HEDGE.get(), 5, 20);
+	}
+
 	private static void registerDispenserBehaviors() {
 		DispenserBlock.registerBehavior(CCItems.CAVEFISH_BUCKET.get(), new FishBucketDispenseItemBehavior());
-		DispenserBlock.registerBehavior(CCItems.SILVER_ARROW.get(), new AbstractProjectileDispenseBehavior() {
+		DispenserBlock.registerBehavior(CCItems.KUNAI.get(), new AbstractProjectileDispenseBehavior() {
 			protected Projectile getProjectile(Level worldIn, Position position, ItemStack stackIn) {
-				SilverArrow entity = new SilverArrow(worldIn, position.x(), position.y(), position.z());
+				Kunai entity = new Kunai(worldIn, position.x(), position.y(), position.z());
 				entity.pickup = AbstractArrow.Pickup.ALLOWED;
 				return entity;
 			}
