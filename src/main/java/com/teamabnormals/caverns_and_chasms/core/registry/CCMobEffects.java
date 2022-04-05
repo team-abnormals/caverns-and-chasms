cd793a18b4cd531b7de7666abb2e480454aa0918
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2022-04-05 14:57:51 +0300

    Simplified Rewind translation strings

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCMobEffects.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCMobEffects.java
@@ -21,8 +21,8 @@ public class CCMobEffects {
 
 	public static final RegistryObject<MobEffect> REWIND = MOB_EFFECTS.register("rewind", () -> new BlueprintMobEffect(MobEffectCategory.BENEFICIAL, 0xCE65B4));
 
-	public static final RegistryObject<Potion> REWIND_NORMAL = POTIONS.register("rewind", () -> new Potion(new MobEffectInstance(REWIND.get(), 400)));
-	public static final RegistryObject<Potion> REWIND_LONG = POTIONS.register("rewind_long", () -> new Potion(new MobEffectInstance(REWIND.get(), 800)));
+	public static final RegistryObject<Potion> REWIND_NORMAL = POTIONS.register("rewind", () -> new Potion("rewind", new MobEffectInstance(REWIND.get(), 400)));
+	public static final RegistryObject<Potion> REWIND_LONG = POTIONS.register("rewind_long", () -> new Potion("rewind", new MobEffectInstance(REWIND.get(), 800)));
 
 	public static void registerBrewingRecipes() {
 		DataUtil.addMix(Potions.AWKWARD, CCItems.BEJEWELED_PEARL.get(), REWIND_NORMAL.get());
