3227665c31e6700ffbac1236d3295d1fb53dc5fe
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2023-02-18 17:53:09 +0200

    Tether potion betterments
    - Tether potions can now have instant effects. The instant effects are applied when the potion is broken.
    - Worn tether potions now use the outer armor model instead of the inner one.
    - Tether potions now appear correctly in the JEI menu.

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/BrewingRecipeRegistryMixin.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/BrewingRecipeRegistryMixin.java
@@ -1,6 +1,5 @@
 package com.teamabnormals.caverns_and_chasms.core.mixin;
 
-import com.teamabnormals.caverns_and_chasms.core.other.CCPotionUtil;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
@@ -14,15 +13,8 @@ public final class BrewingRecipeRegistryMixin {
 
 	@Inject(at = @At("HEAD"), method = "isValidInput", cancellable = true, remap = false)
 	private static void isValidInput(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-		if (stack.getCount() == 1 && stack.getItem() == CCItems.TETHER_POTION.get()) {
+		if (stack.getItem() == CCItems.TETHER_POTION.get()) {
 			cir.setReturnValue(true);
-		}
-	}
-
-	@Inject(at = @At("HEAD"), method = "hasOutput", cancellable = true, remap = false)
-	private static void hasOutput(ItemStack input, ItemStack ingredient, CallbackInfoReturnable<Boolean> cir) {
-		if (input.getItem() == CCItems.TETHER_POTION.get() && CCPotionUtil.isElegantTetherPotion(input)) {
-			cir.setReturnValue(false);
 		}
 	}
 }
--- src/main/java/com/teamabnormals/caverns_and_chasms/integration/jei/CCPlugin.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/integration/jei/CCPlugin.java
@@ -2,12 +2,20 @@ package com.teamabnormals.caverns_and_chasms.integration.jei;
 
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCBlocks;
+import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.ISubtypeRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+
+import java.util.List;
 
 @JeiPlugin
 public class CCPlugin implements IModPlugin {
@@ -20,5 +28,23 @@ public class CCPlugin implements IModPlugin {
 	@Override
 	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
 		registration.addRecipeCatalyst(new ItemStack(CCBlocks.CUPRIC_CAMPFIRE.get()), RecipeTypes.CAMPFIRE_COOKING);
+	}
+
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistration registration) {
+		registration.registerSubtypeInterpreter(CCItems.TETHER_POTION.get(), (itemStack, context) -> {
+			if (!itemStack.hasTag()) {
+				return IIngredientSubtypeInterpreter.NONE;
+			}
+			Potion potionType = PotionUtils.getPotion(itemStack);
+			String potionTypeString = potionType.getName("");
+			StringBuilder stringBuilder = new StringBuilder(potionTypeString);
+			List<MobEffectInstance> effects = PotionUtils.getMobEffects(itemStack);
+			for (MobEffectInstance effect : effects) {
+				stringBuilder.append(";").append(effect);
+			}
+
+			return stringBuilder.toString();
+		});
 	}
 }
