454f5304a3b659b3cb77f40eab938e9b3c198243
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-02 08:47:51 -0600

    Fixes #32
    https://github.com/team-abnormals/indev-issues/issues/32

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/data/client/CCItemModelProvider.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/data/client/CCItemModelProvider.java
@@ -2,7 +2,6 @@ package com.teamabnormals.caverns_and_chasms.core.data.client;
 
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCBlocks;
-import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.ItemLike;
@@ -11,6 +10,8 @@ import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile.UncheckedModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.teamabnormals.caverns_and_chasms.core.registry.CCItems.*;
 
 
 public class CCItemModelProvider extends ItemModelProvider {
@@ -21,28 +22,32 @@ public class CCItemModelProvider extends ItemModelProvider {
 
 	@Override
 	protected void registerModels() {
-		this.generated(CCItems.COPPER_NUGGET.get());
-		this.generated(CCItems.RAW_SILVER.get());
-		this.generated(CCItems.BEJEWELED_APPLE.get());
-		this.generated(CCBlocks.CUPRIC_CANDLE.get());
-		this.animatedModel(CCItems.DEPTH_GAUGE.get(), 48);
-		this.animatedModel(CCItems.BAROMETER.get(), 21);
-		this.generated(CCItems.OXIDIZED_COPPER_GOLEM.get());
-		this.generated(CCItems.AZALEA_BOAT.getFirst().get());
-		this.generated(CCItems.AZALEA_BOAT.getSecond().get());
-		this.generated(CCItems.AZALEA_FURNACE_BOAT.get());
-		this.generated(CCItems.LARGE_AZALEA_BOAT.get());
+		this.generatedItem(CCBlocks.CUPRIC_CANDLE.get());
+		this.animatedModel(DEPTH_GAUGE.get(), 48);
+		this.animatedModel(BAROMETER.get(), 21);
+		this.generatedItem(
+				COPPER_NUGGET.get(), OXIDIZED_COPPER_GOLEM.get(),
+				RAW_SILVER.get(),
+				BEJEWELED_APPLE.get(),
+				AZALEA_BOAT.getFirst().get(), AZALEA_BOAT.getSecond().get(), AZALEA_FURNACE_BOAT.get(), LARGE_AZALEA_BOAT.get()
+		);
+		this.handheldItem(
+				KUNAI.get(),
+				BLUNT_ARROW.get()
+		);
 	}
 
-	private void generated(ItemLike item) {
-		basicModel(item, "generated");
+	private void generatedItem(ItemLike... items) {
+		for (ItemLike item : items)
+			item(item, "generated");
 	}
 
-	private void handheld(ItemLike item) {
-		basicModel(item, "handheld");
+	private void handheldItem(ItemLike... items) {
+		for (ItemLike item : items)
+			item(item, "handheld");
 	}
 
-	private void basicModel(ItemLike item, String type) {
+	private void item(ItemLike item, String type) {
 		ResourceLocation itemName = ForgeRegistries.ITEMS.getKey(item.asItem());
 		withExistingParent(itemName.getPath(), "item/" + type).texture("layer0", new ResourceLocation(this.modid, "item/" + itemName.getPath()));
 	}
