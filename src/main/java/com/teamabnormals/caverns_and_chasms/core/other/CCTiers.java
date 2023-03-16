ffd93997a3bf41402b502f3c86d26b5dd648f1bf
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-16 15:32:33 -0500

    Add repair info to JEI

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCTiers.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCTiers.java
@@ -3,6 +3,7 @@ package com.teamabnormals.caverns_and_chasms.core.other;
 import com.teamabnormals.blueprint.core.api.BlueprintArmorMaterial;
 import com.teamabnormals.blueprint.core.api.BlueprintItemTier;
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
+import com.teamabnormals.caverns_and_chasms.core.other.tags.CCItemTags;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
@@ -13,13 +14,13 @@ import net.minecraft.world.item.crafting.Ingredient;
 public class CCTiers {
 
 	public static class CCArmorMaterials {
-		public static final ArmorMaterial SILVER = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "silver"), 10, new int[]{1, 3, 5, 2}, 20, () -> SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> Ingredient.of(CCItems.SILVER_INGOT.get()));
-		public static final ArmorMaterial NECROMIUM = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "necromium"), 37, new int[]{3, 6, 8, 3}, 13, () -> SoundEvents.ARMOR_EQUIP_NETHERITE, 2.0F, 0.0F, () -> Ingredient.of(CCItems.NECROMIUM_INGOT.get()));
+		public static final ArmorMaterial SILVER = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "silver"), 10, new int[]{1, 3, 5, 2}, 20, () -> SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> Ingredient.of(CCItemTags.INGOTS_SILVER));
+		public static final ArmorMaterial NECROMIUM = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "necromium"), 37, new int[]{3, 6, 8, 3}, 13, () -> SoundEvents.ARMOR_EQUIP_NETHERITE, 2.0F, 0.0F, () -> Ingredient.of(CCItemTags.INGOTS_NECROMIUM));
 		public static final ArmorMaterial SANGUINE = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "sanguine"), 15, new int[]{2, 5, 6, 2}, 12, () -> SoundEvents.ARMOR_EQUIP_IRON, 1.0F, 0.0F, () -> Ingredient.of(CCItems.SANGUINE_PLATING.get()));
 	}
 
 	public static class CCItemTiers {
-		public static final Tier SILVER = new BlueprintItemTier(0, 111, 7.0F, 0.0F, 17, () -> Ingredient.of(CCItems.SILVER_INGOT.get()));
-		public static final Tier NECROMIUM = new BlueprintItemTier(4, 2031, 9.0F, 3.0F, 13, () -> Ingredient.of(CCItems.NECROMIUM_INGOT.get()));
+		public static final Tier SILVER = new BlueprintItemTier(0, 111, 7.0F, 0.0F, 17, () -> Ingredient.of(CCItemTags.INGOTS_SILVER));
+		public static final Tier NECROMIUM = new BlueprintItemTier(4, 2031, 9.0F, 3.0F, 13, () -> Ingredient.of(CCItemTags.INGOTS_NECROMIUM));
 	}
 }
--- src/main/java/com/teamabnormals/caverns_and_chasms/integration/jei/CCPlugin.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/integration/jei/CCPlugin.java
@@ -1,21 +1,28 @@
 package com.teamabnormals.caverns_and_chasms.integration.jei;
 
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
+import com.teamabnormals.caverns_and_chasms.core.other.CCTiers.CCArmorMaterials;
+import com.teamabnormals.caverns_and_chasms.core.other.CCTiers.CCItemTiers;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCBlocks;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.recipe.vanilla.IJeiAnvilRecipe;
+import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @JeiPlugin
 public class CCPlugin implements IModPlugin {
@@ -31,6 +38,7 @@ public class CCPlugin implements IModPlugin {
 	}
 
 	@Override
+
 	public void registerItemSubtypes(ISubtypeRegistration registration) {
 		registration.registerSubtypeInterpreter(CCItems.TETHER_POTION.get(), (itemStack, context) -> {
 			if (!itemStack.hasTag()) {
@@ -46,5 +54,87 @@ public class CCPlugin implements IModPlugin {
 
 			return stringBuilder.toString();
 		});
+	}
+
+	@Override
+	public void registerRecipes(IRecipeRegistration registration) {
+		registration.addRecipes(RecipeTypes.ANVIL, getRepairRecipes(registration.getVanillaRecipeFactory()).toList());
+	}
+
+	private static Stream<RepairData> getRepairData() {
+		return Stream.of(
+				new RepairData(CCArmorMaterials.SANGUINE.getRepairIngredient(),
+						new ItemStack(CCItems.SANGUINE_HELMET.get()),
+						new ItemStack(CCItems.SANGUINE_CHESTPLATE.get()),
+						new ItemStack(CCItems.SANGUINE_LEGGINGS.get()),
+						new ItemStack(CCItems.SANGUINE_BOOTS.get())),
+				new RepairData(CCItemTiers.SILVER.getRepairIngredient(),
+						new ItemStack(CCItems.SILVER_SWORD.get()),
+						new ItemStack(CCItems.SILVER_PICKAXE.get()),
+						new ItemStack(CCItems.SILVER_AXE.get()),
+						new ItemStack(CCItems.SILVER_SHOVEL.get()),
+						new ItemStack(CCItems.SILVER_HOE.get())),
+				new RepairData(CCArmorMaterials.SILVER.getRepairIngredient(),
+						new ItemStack(CCItems.SILVER_HELMET.get()),
+						new ItemStack(CCItems.SILVER_CHESTPLATE.get()),
+						new ItemStack(CCItems.SILVER_LEGGINGS.get()),
+						new ItemStack(CCItems.SILVER_BOOTS.get())),
+				new RepairData(CCItemTiers.NECROMIUM.getRepairIngredient(),
+						new ItemStack(CCItems.NECROMIUM_SWORD.get()),
+						new ItemStack(CCItems.NECROMIUM_AXE.get()),
+						new ItemStack(CCItems.NECROMIUM_HOE.get()),
+						new ItemStack(CCItems.NECROMIUM_SHOVEL.get()),
+						new ItemStack(CCItems.NECROMIUM_PICKAXE.get())),
+				new RepairData(CCArmorMaterials.NECROMIUM.getRepairIngredient(),
+						new ItemStack(CCItems.NECROMIUM_BOOTS.get()),
+						new ItemStack(CCItems.NECROMIUM_HELMET.get()),
+						new ItemStack(CCItems.NECROMIUM_LEGGINGS.get()),
+						new ItemStack(CCItems.NECROMIUM_CHESTPLATE.get())));
+	}
+
+	private static Stream<IJeiAnvilRecipe> getRepairRecipes(IVanillaRecipeFactory vanillaRecipeFactory) {
+		return getRepairData().flatMap(repairData -> getRepairRecipes(repairData, vanillaRecipeFactory));
+	}
+
+	private static Stream<IJeiAnvilRecipe> getRepairRecipes(RepairData repairData, IVanillaRecipeFactory vanillaRecipeFactory) {
+		Ingredient repairIngredient = repairData.getRepairIngredient();
+		List<ItemStack> repairables = repairData.getRepairables();
+
+		List<ItemStack> repairMaterials = List.of(repairIngredient.getItems());
+
+		return repairables.stream().mapMulti((itemStack, consumer) -> {
+			ItemStack damagedThreeQuarters = itemStack.copy();
+			damagedThreeQuarters.setDamageValue(damagedThreeQuarters.getMaxDamage() * 3 / 4);
+			ItemStack damagedHalf = itemStack.copy();
+			damagedHalf.setDamageValue(damagedHalf.getMaxDamage() / 2);
+
+			IJeiAnvilRecipe repairWithSame = vanillaRecipeFactory.createAnvilRecipe(List.of(damagedThreeQuarters), List.of(damagedThreeQuarters), List.of(damagedHalf));
+			consumer.accept(repairWithSame);
+
+			if (!repairMaterials.isEmpty()) {
+				ItemStack damagedFully = itemStack.copy();
+				damagedFully.setDamageValue(damagedFully.getMaxDamage());
+				IJeiAnvilRecipe repairWithMaterial = vanillaRecipeFactory.createAnvilRecipe(List.of(damagedFully), repairMaterials, List.of(damagedThreeQuarters));
+				consumer.accept(repairWithMaterial);
+			}
+		});
+	}
+
+	private static class RepairData {
+		private final Ingredient repairIngredient;
+		private final List<ItemStack> repairables;
+
+		public RepairData(Ingredient repairIngredient, ItemStack... repairables) {
+			this.repairIngredient = repairIngredient;
+			this.repairables = List.of(repairables);
+		}
+
+		public Ingredient getRepairIngredient() {
+			return repairIngredient;
+		}
+
+		public List<ItemStack> getRepairables() {
+			return repairables;
+		}
 	}
 }
