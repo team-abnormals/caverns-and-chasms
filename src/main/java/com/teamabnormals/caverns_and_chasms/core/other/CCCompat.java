a69d5709bfa1e933975e69b2c3383a3d3c1ada23
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2024-03-06 22:50:06 +0200

    Fixed #25

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCCompat.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCCompat.java
@@ -56,6 +56,7 @@ public class CCCompat {
 	private static void registerDispenserBehaviors() {
 		DispenserBlock.registerBehavior(CCItems.KUNAI.get(), new KunaiDispenseBehavior());
 		DispenserBlock.registerBehavior(CCItems.BLUNT_ARROW.get(), new BluntArrowDispenseBehavior());
+		DispenserBlock.registerBehavior(CCItems.LARGE_ARROW.get(), new LargeArrowDispenserBehavior());
 		DispenserBlock.registerBehavior(CCBlocks.TMT.get(), new TMTDispenseBehavior());
 		DispenserBlock.registerBehavior(CCItems.GOLDEN_BUCKET.get(), new GoldenBucketDispenseBehavior());
 
