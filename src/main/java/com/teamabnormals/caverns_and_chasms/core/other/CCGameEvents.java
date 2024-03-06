96a3a76f25d59f9ea4cc4f528b36b42e7413c2ad
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2024-03-07 00:40:23 +0200

    Made game event registering better

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCGameEvents.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCGameEvents.java
@@ -2,6 +2,7 @@ package com.teamabnormals.caverns_and_chasms.core.other;
 
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -9,5 +10,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class CCGameEvents {
 	public static final DeferredRegister<GameEvent> GAME_EVENTS = DeferredRegister.create(Registry.GAME_EVENT_REGISTRY, CavernsAndChasms.MOD_ID);
 
-	public static final RegistryObject<GameEvent> TUNING_FORK_VIBRATE = GAME_EVENTS.register("tuning_fork_vibrate", () -> new GameEvent("tuning_fork_vibrate", 16));
+	public static final RegistryObject<GameEvent> TUNING_FORK_VIBRATE = register("tuning_fork_vibrate", 16);
+
+	public static RegistryObject<GameEvent> register(String name, int radius) {
+		return GAME_EVENTS.register(name, () -> new GameEvent(new ResourceLocation(CavernsAndChasms.MOD_ID, name).toString(), radius));
+	}
 }
