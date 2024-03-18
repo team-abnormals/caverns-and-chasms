175436a5dee9f751ff6847cdea1a21e1e6b994be
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-03-18 10:00:06 -0500

    Fix Silver particles on server
    Closes #22

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/item/silver/SilverItem.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/item/silver/SilverItem.java
@@ -2,8 +2,15 @@ package com.teamabnormals.caverns_and_chasms.common.item.silver;
 
 import com.teamabnormals.blueprint.core.util.NetworkUtil;
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
+import com.teamabnormals.caverns_and_chasms.core.registry.CCParticleTypes;
+import net.minecraft.client.player.RemotePlayer;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
+
+import java.rmi.Remote;
 
 public class SilverItem {
 
@@ -21,12 +28,25 @@ public class SilverItem {
 		if (defensive) {
 			count += 3 + random.nextInt(2);
 		}
-		for (int i = 0; i < count; ++i) {
-			double d0 = random.nextGaussian() * 0.02D;
-			double d1 = random.nextGaussian() * 0.02D;
-			double d2 = random.nextGaussian() * 0.02D;
+		if (entity.level.isClientSide()) {
+			ParticleOptions particle = defensive ? CCParticleTypes.SILVER_SPARK.get() : CCParticleTypes.SILVER_HIT.get();
+			for (int i = 0; i < count; ++i) {
+				double d0 = random.nextGaussian() * 0.02D;
+				double d1 = random.nextGaussian() * 0.02D;
+				double d2 = random.nextGaussian() * 0.02D;
+				entity.level.addParticle(particle, entity.getRandomX(0.75D), entity.getRandomY() + (defensive ? 0.3F : 0.0F), entity.getRandomZ(0.75D), d0, d1, d2);
+			}
+
+		} else {
 			String particle = CavernsAndChasms.MOD_ID + ":silver_" + (defensive ? "spark" : "hit");
-			NetworkUtil.spawnParticle(particle, entity.getRandomX(0.75D), entity.getRandomY() + (defensive ? 0.3F : 0.0F), entity.getRandomZ(0.75D), d0, d1, d2);
+			for (int i = 0; i < count; ++i) {
+				double d0 = random.nextGaussian() * 0.02D;
+				double d1 = random.nextGaussian() * 0.02D;
+				double d2 = random.nextGaussian() * 0.02D;
+				NetworkUtil.spawnParticle(particle, entity.getRandomX(0.75D), entity.getRandomY() + (defensive ? 0.3F : 0.0F), entity.getRandomZ(0.75D), d0, d1, d2);
+			}
 		}
+
+
 	}
 }
