2ed9758641764874b39ccc56018b1e19e1852a67
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-27 10:37:01 -0500

    Make tiers more consistent

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCTiers.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCTiers.java
@@ -14,13 +14,13 @@ import net.minecraft.world.item.crafting.Ingredient;
 public class CCTiers {
 
 	public static class CCArmorMaterials {
-		public static final ArmorMaterial SILVER = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "silver"), 10, new int[]{1, 3, 5, 2}, 20, () -> SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> Ingredient.of(CCItemTags.INGOTS_SILVER));
-		public static final ArmorMaterial NECROMIUM = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "necromium"), 37, new int[]{3, 6, 8, 3}, 13, () -> SoundEvents.ARMOR_EQUIP_NETHERITE, 2.0F, 0.0F, () -> Ingredient.of(CCItemTags.INGOTS_NECROMIUM));
-		public static final ArmorMaterial SANGUINE = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "sanguine"), 15, new int[]{2, 5, 6, 2}, 12, () -> SoundEvents.ARMOR_EQUIP_IRON, 1.0F, 0.0F, () -> Ingredient.of(CCItems.LIVING_FLESH.get()));
+		public static final ArmorMaterial SILVER = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "silver"), 11, new int[]{2, 4, 5, 2}, 17, () -> SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> Ingredient.of(CCItemTags.INGOTS_SILVER));
+		public static final ArmorMaterial NECROMIUM = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "necromium"), 37, new int[]{3, 6, 8, 3}, 15, () -> SoundEvents.ARMOR_EQUIP_NETHERITE, 2.0F, 0.0F, () -> Ingredient.of(CCItemTags.INGOTS_NECROMIUM));
+		public static final ArmorMaterial SANGUINE = new BlueprintArmorMaterial(new ResourceLocation(CavernsAndChasms.MOD_ID, "sanguine"), 23, new int[]{2, 5, 7, 3}, 17, () -> SoundEvents.ARMOR_EQUIP_IRON, 1.0F, 0.0F, () -> Ingredient.of(CCItems.LIVING_FLESH.get()));
 	}
 
 	public static class CCItemTiers {
-		public static final Tier SILVER = new BlueprintItemTier(0, 111, 7.0F, 0.0F, 17, () -> Ingredient.of(CCItemTags.INGOTS_SILVER));
-		public static final Tier NECROMIUM = new BlueprintItemTier(4, 2031, 9.0F, 3.0F, 13, () -> Ingredient.of(CCItemTags.INGOTS_NECROMIUM));
+		public static final Tier SILVER = new BlueprintItemTier(1, 157, 9.0F, 1.0F, 18, () -> Ingredient.of(CCItemTags.INGOTS_SILVER));
+		public static final Tier NECROMIUM = new BlueprintItemTier(4, 2031, 9.0F, 3.0F, 15, () -> Ingredient.of(CCItemTags.INGOTS_NECROMIUM));
 	}
 }
