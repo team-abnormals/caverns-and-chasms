d620c38db2066fad5c9fa4049447d90b79eac1f9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-05-02 13:24:30 -0500

    Add Noir and Starry Night paintings

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCPaintingVariants.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCPaintingVariants.java
@@ -12,5 +12,6 @@ public final class CCPaintingVariants {
 	public static final RegistryObject<PaintingVariant> ISOLATION = PAINTING_VARIANTS.register("isolation", () -> new PaintingVariant(32, 32));
 	public static final RegistryObject<PaintingVariant> EXSANGUINATED = PAINTING_VARIANTS.register("exsanguinated", () -> new PaintingVariant(64, 64));
 	public static final RegistryObject<PaintingVariant> EMBEDDED = PAINTING_VARIANTS.register("embedded", () -> new PaintingVariant(32, 64));
-
+	public static final RegistryObject<PaintingVariant> STARRY_NIGHT = PAINTING_VARIANTS.register("starry_night", () -> new PaintingVariant(16, 16));
+	public static final RegistryObject<PaintingVariant> NOIR = PAINTING_VARIANTS.register("noir", () -> new PaintingVariant(32, 16));
 }
