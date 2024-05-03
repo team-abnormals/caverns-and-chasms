c1dc600fdb3897f723cd3a5865e949d1b00b9dfe
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-05-02 22:02:55 -0500

    fix

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCPaintingVariants.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCPaintingVariants.java
@@ -13,5 +13,5 @@ public final class CCPaintingVariants {
 	public static final RegistryObject<PaintingVariant> EXSANGUINATED = PAINTING_VARIANTS.register("exsanguinated", () -> new PaintingVariant(64, 64));
 	public static final RegistryObject<PaintingVariant> EMBEDDED = PAINTING_VARIANTS.register("embedded", () -> new PaintingVariant(32, 64));
 	public static final RegistryObject<PaintingVariant> STARRY_NIGHT = PAINTING_VARIANTS.register("starry_night", () -> new PaintingVariant(16, 16));
-	public static final RegistryObject<PaintingVariant> NOIR = PAINTING_VARIANTS.register("noir", () -> new PaintingVariant(32, 16));
+	public static final RegistryObject<PaintingVariant> NOIR = PAINTING_VARIANTS.register("noir", () -> new PaintingVariant(16, 32));
 }
