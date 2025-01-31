42bdedbe9dcf1095188280d66dd6e9920bf970c5
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-01-30 23:06:36 -0600

    idk man lmao

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/item/CaviarItem.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/item/CaviarItem.java
@@ -1,5 +1,7 @@
 package com.teamabnormals.caverns_and_chasms.common.item;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -21,5 +23,9 @@ public class CaviarItem extends Item {
 		}
 
 		return copy;
+	}
+
+	public SoundEvent getEatingSound() {
+		return SoundEvents.PLAYER_BURP;
 	}
 }
