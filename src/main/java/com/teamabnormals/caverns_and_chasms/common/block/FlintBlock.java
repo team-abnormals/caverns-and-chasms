de33ab267810885a877b26a266497926101148f2
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2025-02-01 21:59:02 +0400

    Spark particles

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/block/FlintBlock.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/block/FlintBlock.java
@@ -1,7 +1,10 @@
 package com.teamabnormals.caverns_and_chasms.common.block;
 
+import com.teamabnormals.blueprint.core.util.NetworkUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.FallingBlockEntity;
@@ -9,7 +12,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
-
+import net.minecraft.world.phys.Vec3;
 
 
 public class FlintBlock extends FallingBlock {
@@ -53,6 +56,14 @@ public class FlintBlock extends FallingBlock {
 				if (i != 0 || j != 0 ) {
 					BlockPos randomPos = pos.offset(i, 0, j);
 					BlockState firestate = BaseFireBlock.getState(level, randomPos);
+					Vec3 direction = new Vec3(randomPos.getX() - pos.getX(), randomPos.getY(), randomPos.getZ() - pos.getZ()).normalize();
+					for (int l = 0; l < 10; ++l) {
+						double d1 = direction.x * 0.4D + level.random.nextGaussian() * 0.05D;
+						double d2 = direction.y * 0.4D + level.random.nextGaussian() * 0.05D;
+						double d3 = direction.z * 0.4D + level.random.nextGaussian() * 0.05D;
+						
+						NetworkUtil.spawnParticle("caverns_and_chasms:spark", pos.getX(), pos.getY(), pos.getZ(), d1, d2, d3);
+					}
 					if (level.getBlockState(randomPos).isAir() && firestate.canSurvive(level, randomPos)) {
 						level.setBlockAndUpdate(randomPos, firestate);
 					}
