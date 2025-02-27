a688b0c75c546d1cce3f207cd412e74ee4da2382
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2025-02-27 19:05:16 +0400

    Update scatterer behaviors

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/block/ScattererBlock.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/block/ScattererBlock.java
@@ -1,13 +1,15 @@
 package com.teamabnormals.caverns_and_chasms.common.block;
 
 import com.teamabnormals.caverns_and_chasms.common.block.entity.ScattererBlockEntity;
+import com.teamabnormals.caverns_and_chasms.common.dispenser.FireChargeScattererBehavior;
+import com.teamabnormals.caverns_and_chasms.common.dispenser.FireworkScattererBehavior;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockSourceImpl;
 import net.minecraft.core.Direction;
 import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DispenserBlock;
@@ -15,21 +17,19 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.DispenserBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 
 public class ScattererBlock extends DispenserBlock {
-	public static final IntegerProperty POWER = BlockStateProperties.POWER;
-
 	public ScattererBlock(Properties properties) {
 		super(properties);
-		this.registerDefaultState(this.stateDefinition.any().setValue(POWER, 0).setValue(FACING, Direction.NORTH).setValue(TRIGGERED, false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(TRIGGERED, false));
 	}
 
 	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
 		return new ScattererBlockEntity(pos, state);
 	}
+
+	public int powerLevel;
 
 	@Override
 	public void neighborChanged(BlockState state, Level level, BlockPos pos, Block p_52703_, BlockPos p_52704_, boolean p_52705_) {
@@ -37,18 +37,11 @@ public class ScattererBlock extends DispenserBlock {
 		boolean flag1 = state.getValue(TRIGGERED);
 		if (flag && !flag1) {
 			level.scheduleTick(pos, this, 4);
-			level.setBlock(pos, state.setValue(TRIGGERED, true).setValue(POWER, level.getBestNeighborSignal(pos)), 3);
+			powerLevel = level.getBestNeighborSignal(pos);
+			level.setBlock(pos, state.setValue(TRIGGERED, true), 3);
 		} else if (!flag && flag1) {
-			level.setBlock(pos, state.setValue(TRIGGERED, false).setValue(POWER, 0), 3);
+			level.setBlock(pos, state.setValue(TRIGGERED, false), 3);
 		}
-	}
-
-	@Override
-	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-		if (level.getBestNeighborSignal(pos) != state.getValue(POWER)) {
-			level.setBlock(pos, state.setValue(TRIGGERED, true).setValue(POWER, level.getBestNeighborSignal(pos)), 3);
-		}
-		super.animateTick(state, level, pos, random);
 	}
 
 	@Override
@@ -57,7 +50,7 @@ public class ScattererBlock extends DispenserBlock {
 		DispenserBlockEntity dispenserblockentity = blocksourceimpl.getEntity();
 		for (int i = 0; i < dispenserblockentity.getContainerSize(); i++) {
 			ItemStack itemstack = dispenserblockentity.getItem(i);
-			DispenseItemBehavior dispenseitembehavior = this.getDispenseMethod(itemstack);
+			DispenseItemBehavior dispenseitembehavior = getScatterMethod(itemstack);
 			if (dispenseitembehavior != DispenseItemBehavior.NOOP) {
 				dispenserblockentity.setItem(i, dispenseitembehavior.dispense(blocksourceimpl, itemstack));
 			}
@@ -66,16 +59,18 @@ public class ScattererBlock extends DispenserBlock {
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(POWER, TRIGGERED, FACING);
+		builder.add(TRIGGERED, FACING);
 	}
 
-	@Override
-	public int getAnalogOutputSignal(BlockState state, Level level, BlockPos pos) {
-		return state.getValue(POWER);
-	}
-
-	@Override
-	public boolean hasAnalogOutputSignal(BlockState state) {
-		return true;
+	public DispenseItemBehavior getScatterMethod(ItemStack stack) {
+		if (stack.getItem() == Items.FIREWORK_ROCKET) {
+			return new FireworkScattererBehavior();
+		}
+		if (stack.getItem() == Items.FIRE_CHARGE) {
+			return new FireChargeScattererBehavior();
+		}
+		else {
+			return 	this.getDispenseMethod(stack);
+		}
 	}
 }
