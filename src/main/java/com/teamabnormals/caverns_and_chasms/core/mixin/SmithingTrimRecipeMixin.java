ee1e9bc4be6fe3049dac10944184c94696e32583
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-03-01 13:25:20 -0600

    Fixes #101

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/SmithingTrimRecipeMixin.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/SmithingTrimRecipeMixin.java
@@ -1,13 +1,11 @@
 package com.teamabnormals.caverns_and_chasms.core.mixin;
 
 import com.teamabnormals.caverns_and_chasms.core.CCConfig;
-import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.armortrim.TrimPattern;
-import net.minecraft.world.item.armortrim.TrimPatterns;
+import net.minecraft.world.item.armortrim.ArmorTrim;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.SmithingTrimRecipe;
 import org.spongepowered.asm.mixin.Final;
@@ -29,7 +27,7 @@ public class SmithingTrimRecipeMixin {
 	@Inject(method = "assemble", at = @At(value = "RETURN", target = "Lnet/minecraft/world/item/armortrim/ArmorTrim;setTrim(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/armortrim/ArmorTrim;)Z"), cancellable = true)
 	private void assemble(Container container, RegistryAccess access, CallbackInfoReturnable<ItemStack> cir) {
 		if (this.base.test(container.getItem(1))) {
-			Optional<Holder.Reference<TrimPattern>> pattern = TrimPatterns.getFromTemplate(access, container.getItem(0));
+			Optional<ArmorTrim> pattern = ArmorTrim.getTrim(access, container.getItem(1));
 			if (pattern.isPresent() && CCConfig.COMMON.preventReplacingTrims.get()) {
 				cir.setReturnValue(ItemStack.EMPTY);
 			}
