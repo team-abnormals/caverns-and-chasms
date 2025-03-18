82834c06b2f9a54bb080ffc2664d72e7ace234bb
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-03-18 00:29:36 -0500

    Reworked Gravel Pile decorations

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCStructureTypes.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCStructureTypes.java
@@ -7,7 +7,6 @@ import com.teamabnormals.caverns_and_chasms.common.levelgen.structure.TinMonolit
 import com.teamabnormals.caverns_and_chasms.common.levelgen.structure.TinMonolithStructure;
 import com.teamabnormals.caverns_and_chasms.core.CavernsAndChasms;
 import com.teamabnormals.caverns_and_chasms.core.data.server.CCLootTableProvider.CCArchaeologyLoot;
-import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Holder.Reference;
 import net.minecraft.core.HolderGetter;
@@ -19,9 +18,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -71,9 +69,6 @@ public class CCStructureTypes {
 			float uncommon = 0.02F;
 			float common = 0.04F;
 
-			BlockState toolbox = CCBlocks.WAXED_TOOLBOX.get().defaultBlockState();
-			BlockState button = Blocks.STONE_BUTTON.defaultBlockState().setValue(ButtonBlock.FACE, AttachFace.FLOOR);
-
 			register(context, FORGE_ARCHAEOLOGY, ImmutableList.of(
 					new RuleProcessor(ImmutableList.of(
 							replaceGravelWith(CCBlocks.TURQUOISE_ORE.get(), legendary),
@@ -98,44 +93,8 @@ public class CCStructureTypes {
 					)),
 
 					archyLootProcessor(CCArchaeologyLoot.FORGE_RARE, 1),
-					archyLootProcessor(CCArchaeologyLoot.FORGE_COMMON, 5),
-
-					new RuleProcessor(ImmutableList.of(
-							addDecor(Blocks.AIR, 0.75F),
-							addDecor(Blocks.BROWN_MUSHROOM, 0.02F),
-							addDecor(Blocks.RED_MUSHROOM, 0.02F),
-							addDecor(CCBlocks.CAVE_GROWTHS.get(), 0.005F),
-							addDecor(CCBlocks.LURID_CAVE_GROWTHS.get(), 0.005F),
-							addDecor(CCBlocks.WISPY_CAVE_GROWTHS.get(), 0.005F),
-							addDecor(CCBlocks.GRAINY_CAVE_GROWTHS.get(), 0.005F),
-							addDecor(CCBlocks.WEIRD_CAVE_GROWTHS.get(), 0.005F),
-							addDecor(CCBlocks.ZESTY_CAVE_GROWTHS.get(), 0.005F),
-							addDecor(toolbox, Direction.NORTH, 0.015F),
-							addDecor(toolbox, Direction.SOUTH, 0.015F),
-							addDecor(toolbox, Direction.EAST, 0.015F),
-							addDecor(toolbox, Direction.WEST, 0.015F),
-							addDecor(button, Direction.NORTH, 0.075F),
-							addDecor(button, Direction.SOUTH, 0.075F),
-							addDecor(button, Direction.EAST, 0.075F),
-							addDecor(button, Direction.WEST, 0.075F),
-							addDecor(Blocks.CANDLE.defaultBlockState().setValue(CandleBlock.CANDLES, 1), 0.02F),
-							addDecor(Blocks.CANDLE.defaultBlockState().setValue(CandleBlock.CANDLES, 2), 0.02F),
-							addDecor(Blocks.CANDLE.defaultBlockState().setValue(CandleBlock.CANDLES, 3), 0.02F),
-							addDecor(Blocks.CANDLE.defaultBlockState().setValue(CandleBlock.CANDLES, 4), 0.02F)
-					))
+					archyLootProcessor(CCArchaeologyLoot.FORGE_COMMON, 5)
 			));
-		}
-
-		private static ProcessorRule addDecor(BlockState block, Direction direction, float chance) {
-			return addDecor(block.setValue(HorizontalDirectionalBlock.FACING, direction), chance);
-		}
-
-		private static ProcessorRule addDecor(Block block, float chance) {
-			return addDecor(block.defaultBlockState(), chance);
-		}
-
-		private static ProcessorRule addDecor(BlockState block, float chance) {
-			return new ProcessorRule(new RandomBlockMatchTest(Blocks.STONE_PRESSURE_PLATE, chance), AlwaysTrueTest.INSTANCE, block);
 		}
 
 		private static ProcessorRule replaceGravelWith(Block block, float chance) {
@@ -162,14 +121,16 @@ public class CCStructureTypes {
 	public static class CCTemplatePools {
 		public static final ResourceKey<StructureTemplatePool> FORGE = createKey("forge/forge");
 		public static final ResourceKey<StructureTemplatePool> FORGE_ENTRANCES = createKey("forge/entrances");
+		public static final ResourceKey<StructureTemplatePool> FORGE_ARCHAEOLOGY = createKey("forge/archaeology");
 		public static final ResourceKey<StructureTemplatePool> FORGE_DECORATIONS = createKey("forge/decorations");
 		public static final ResourceKey<StructureTemplatePool> FORGE_SMALL_DECORATIONS = createKey("forge/small_decorations");
-		public static final ResourceKey<StructureTemplatePool> FORGE_ARCHAEOLOGY = createKey("forge/archaeology");
+		public static final ResourceKey<StructureTemplatePool> FORGE_PILE_DECORATIONS = createKey("forge/pile_decorations");
 
-		public static final List<Pair<String, Integer>> ENTRANCES = List.of(Pair.of("gate", 6), Pair.of("broken_gate", 4));
-		public static final List<Pair<String, Integer>> DECORATIONS = List.of(Pair.of("oak_platform", 8), Pair.of("oak_shelf", 2), Pair.of("tnt_pile", 3));
-		public static final List<Pair<String, Integer>> SMALL_DECORATIONS = List.of(Pair.of("cauldron", 1), Pair.of("furnace", 1), Pair.of("blast_furnace", 1), Pair.of("cauldron", 1), Pair.of("dimmer", 1), Pair.of("dimmer_scaffolding", 1), Pair.of("anvil", 1), Pair.of("tnt", 2), Pair.of("tnt_scaffolding", 2), Pair.of("water_cauldron", 3), Pair.of("scaffolding", 4));
-		public static final List<Pair<String, Integer>> ARCHAEOLOGY = List.of(Pair.of("gravel_pile", 32));
+		public static final List<Entry> ENTRANCES = List.of(of("gate", 6), of("broken_gate", 4));
+		public static final List<Entry> ARCHAEOLOGY = List.of(of("gravel_pile", 32));
+		public static final List<Entry> DECORATIONS = List.of(of("oak_platform", 8), of("oak_shelf", 2), of("tnt_pile", 3));
+		public static final List<Entry> SMALL_DECORATIONS = List.of(of("empty", 1, 17), of("cauldron", 1), of("furnace", 1), of("blast_furnace", 1), of("damaged_anvil", 1), of("dimmer", 1), of("dimmer_scaffolding", 1), of("anvil", 1), of("tnt", 2), of("tnt_scaffolding", 2), of("water_cauldron", 3), of("scaffolding", 4));
+		public static final List<Entry> PILE_DECORATIONS = List.of(of("empty", 1, 150), of("stone_button", 1, 60), of("mushroom", 2, 3), of("cave_growths", 6, 1), of("candle", 4, 4), of("coal", 4, 5), of("charcoal", 4, 4), of("toolbox", 1, 8));
 
 		public static void bootstrap(BootstapContext<StructureTemplatePool> context) {
 			Holder<StructureTemplatePool> empty = context.lookup(Registries.TEMPLATE_POOL).getOrThrow(Pools.EMPTY);
@@ -177,28 +138,44 @@ public class CCStructureTypes {
 			context.register(FORGE, new StructureTemplatePool(empty, ImmutableList.of(Pair.of(LegacySinglePoolElement.single(FORGE.location().toString()), 1)), StructureTemplatePool.Projection.RIGID));
 
 			createPool(context, FORGE_ENTRANCES, empty, ENTRANCES);
+			createPool(context, FORGE_ARCHAEOLOGY, empty, ARCHAEOLOGY);
 			createPool(context, FORGE_DECORATIONS, empty, DECORATIONS);
 			createPool(context, FORGE_SMALL_DECORATIONS, empty, SMALL_DECORATIONS);
-			createPool(context, FORGE_ARCHAEOLOGY, empty, ARCHAEOLOGY);
+			createPool(context, FORGE_PILE_DECORATIONS, empty, PILE_DECORATIONS);
 		}
 
-		public static void createPool(BootstapContext<StructureTemplatePool> context, ResourceKey<StructureTemplatePool> key, Holder<StructureTemplatePool> empty, List<Pair<String, Integer>> strs) {
+		public static void createPool(BootstapContext<StructureTemplatePool> context, ResourceKey<StructureTemplatePool> key, Holder<StructureTemplatePool> empty, List<Entry> strs) {
 			boolean processor = FORGE_ARCHAEOLOGY.equals(key);
 			Reference<StructureProcessorList> archyProcessor = context.lookup(Registries.PROCESSOR_LIST).getOrThrow(CCProcessorLists.FORGE_ARCHAEOLOGY);
 
 			List<Pair<Function<Projection, ? extends StructurePoolElement>, Integer>> list = Lists.newArrayList();
-			for (Pair<String, Integer> str : strs) {
-				for (int i = 1; i <= str.getSecond(); i++) {
-					String name = key.location() + "/" + str.getFirst() + "_" + i;
-					list.add(Pair.of(processor ? LegacySinglePoolElement.single(name, archyProcessor) : LegacySinglePoolElement.single(name), 1));
+			for (Entry entry : strs) {
+				if (entry.name().equals("empty")) {
+					list.add(Pair.of(EmptyPoolElement.empty(), entry.weight()));
+				} else {
+					for (int i = 1; i <= entry.count(); i++) {
+						String name = key.location() + "/" + entry.name() + "_" + i;
+						list.add(Pair.of(processor ? LegacySinglePoolElement.single(name, archyProcessor) : LegacySinglePoolElement.single(name), entry.weight()));
+					}
 				}
 			}
 
-			if (FORGE_SMALL_DECORATIONS.equals(key)) {
-				list.add(Pair.of(EmptyPoolElement.empty(), 17));
-			}
-
 			context.register(key, new StructureTemplatePool(empty, ImmutableList.copyOf(list), StructureTemplatePool.Projection.RIGID));
+		}
+
+		public static Entry of(String name, int count, int weight) {
+			return new Entry(name, count, weight);
+		}
+
+		public static Entry of(String name, int count) {
+			return new Entry(name, count);
+		}
+
+		public record Entry(String name, int count, int weight) {
+
+			public Entry(String name, int count) {
+				this(name, count, 1);
+			}
 		}
 
 		public static ResourceKey<StructureTemplatePool> createKey(String name) {
