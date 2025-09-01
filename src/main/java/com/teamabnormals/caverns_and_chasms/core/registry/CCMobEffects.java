662de7d9aaafa38d4eac64d1daf1d8dcb85aa5a7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-08-31 21:46:46 -0500

    Fixes #163

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCMobEffects.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCMobEffects.java
@@ -31,9 +31,11 @@ public class CCMobEffects {
 
 		PotionBrewing.addContainer(CCItems.IMPACT_POTION.get());
 		PotionBrewing.addContainerRecipe(Items.SPLASH_POTION, CCItems.SPINEL.get(), CCItems.IMPACT_POTION.get());
+		PotionBrewing.addContainerRecipe(CCItems.TETHER_POTION.get(), Items.GUNPOWDER, CCItems.IMPACT_POTION.get());
 
 		PotionBrewing.addContainer(CCItems.TRAIL_POTION.get());
 		PotionBrewing.addContainerRecipe(Items.LINGERING_POTION, CCItems.SPINEL.get(), CCItems.TRAIL_POTION.get());
+		PotionBrewing.addContainerRecipe(CCItems.IMPACT_POTION.get(), Items.DRAGON_BREATH, CCItems.TRAIL_POTION.get());
 
 		PotionBrewing.addContainerRecipe(Items.POTION, CCItems.TURQUOISE.get(), Items.POTION);
 		PotionBrewing.addContainerRecipe(Items.SPLASH_POTION, CCItems.TURQUOISE.get(), Items.SPLASH_POTION);
