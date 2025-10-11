10f7bef15f3543328bae54d82a4e1e98dc3b01a7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-10-11 18:21:59 -0500

    Improve Toolbox tooltip

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/block/ToolboxBlock.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/block/ToolboxBlock.java
@@ -13,6 +13,7 @@ import net.minecraft.core.Direction.Axis;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.Container;
@@ -26,6 +27,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
@@ -225,13 +227,24 @@ public class ToolboxBlock extends BaseEntityBlock implements SimpleWaterloggedBl
 						++j;
 						if (i <= 4) {
 							++i;
-							components.add(itemstack.getHoverName().copy());
+
+							MutableComponent name = Component.empty().append(itemstack.getHoverName());
+							if (itemstack.hasCustomHoverName()) {
+								name.withStyle(ChatFormatting.ITALIC);
+							}
+							if (itemstack.getRarity() != Rarity.COMMON) {
+								name.withStyle(itemstack.getRarity().getStyleModifier());
+							} else {
+								name.withStyle(ChatFormatting.GRAY);
+							}
+
+							components.add(name);
 						}
 					}
 				}
 
 				if (j - i > 0) {
-					components.add(Component.translatable("container." + CavernsAndChasms.MOD_ID + ".toolbox.more", j - i).withStyle(ChatFormatting.ITALIC));
+					components.add(Component.translatable("container." + CavernsAndChasms.MOD_ID + ".toolbox.more", j - i).withStyle(ChatFormatting.GRAY));
 				}
 			}
 		}
