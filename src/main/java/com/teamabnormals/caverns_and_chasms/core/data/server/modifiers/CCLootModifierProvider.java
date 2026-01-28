0f346e4bc116828aa33c9141be96cba6121f6010
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-01-28 17:22:37 -0600

    Add Copper Horse Armor to chests

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/data/server/modifiers/CCLootModifierProvider.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/data/server/modifiers/CCLootModifierProvider.java
@@ -39,6 +39,7 @@ public class CCLootModifierProvider extends LootModifierProvider {
 	@Override
 	protected void registerEntries(Provider provider) {
 		this.entry("simple_dungeon").selects(BuiltInLootTables.SIMPLE_DUNGEON)
+				.addModifier(new LootPoolEntriesModifier(false, 0, List.of(lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 15))))
 				.addModifier(new LootPoolEntriesModifier(false, 0, List.of(lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 10))))
 				.addModifier(new LootPoolEntriesModifier(false, 1, List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 4))));
 		this.entry("abandoned_mineshaft").selects(BuiltInLootTables.ABANDONED_MINESHAFT)
@@ -46,7 +47,7 @@ public class CCLootModifierProvider extends LootModifierProvider {
 				.addModifier(new LootPoolEntriesModifier(false, 1, List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 3), lootPool(CCItems.SPINEL.get(), 5, 6, 11))))
 				.addModifier(new LootPoolEntriesModifier(false, 2, List.of(lootPool(CCBlocks.SPIKED_RAIL.get(), 5, 1, 4))));
 		this.entry("stronghold_corridor").selects(BuiltInLootTables.STRONGHOLD_CORRIDOR).addModifier(new LootPoolEntriesModifier(false, 0,
-				List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 3), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1))));
+				List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 3), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1), lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 1))));
 		this.entry("stronghold_crossing").selects(BuiltInLootTables.STRONGHOLD_CROSSING).addModifier(new LootPoolEntriesModifier(false, 0,
 				List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 3))));
 
@@ -59,16 +60,16 @@ public class CCLootModifierProvider extends LootModifierProvider {
 				.addModifier(new LootPoolEntriesModifier(false, 1, List.of(lootPool(CCItems.SILVER_INGOT.get(), 10, 1, 4))));
 
 		this.entry("desert_pyramid").selects(BuiltInLootTables.DESERT_PYRAMID).addModifier(new LootPoolEntriesModifier(false, 0,
-				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 1, 5), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 10))));
+				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 1, 5), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 10), lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 15))));
 		this.entry("jungle_temple").selects(BuiltInLootTables.JUNGLE_TEMPLE).addModifier(new LootPoolEntriesModifier(false, 0,
-				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 2, 7), lootPool(CCItems.SPINEL.get(), 15, 2, 5), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1))));
+				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 2, 7), lootPool(CCItems.SPINEL.get(), 15, 2, 5), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1), lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 1))));
 		this.entry("woodland_mansion").selects(BuiltInLootTables.WOODLAND_MANSION).addModifier(new LootPoolEntriesModifier(false, 1,
 				List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 4))));
 
 		this.entry("village_fisher").selects(BuiltInLootTables.VILLAGE_FISHER)
 				.addModifier(new LootPoolsModifier(List.of(LootPool.lootPool().name(CavernsAndChasms.MOD_ID + ":barometer").setRolls(UniformGenerator.between(0.0F, 1.0F)).add(LootItem.lootTableItem(CCItems.BAROMETER.get())).build()), false));
 		this.entry("village_weaponsmith").selects(BuiltInLootTables.VILLAGE_WEAPONSMITH).addModifier(new LootPoolEntriesModifier(false, 0,
-				List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 3), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1))));
+				List.of(lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 3), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1), lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 1))));
 		this.entry("village_toolsmith").selects(BuiltInLootTables.VILLAGE_TOOLSMITH).addModifier(new LootPoolEntriesModifier(false, 0,
 				List.of(lootPool(CCItems.SILVER_INGOT.get(), 1, 1, 3))));
 		this.entry("village_temple").selects(BuiltInLootTables.VILLAGE_TEMPLE).addModifier(new LootPoolEntriesModifier(false, 0,
@@ -81,16 +82,17 @@ public class CCLootModifierProvider extends LootModifierProvider {
 		this.entry("nether_bridge").selects(BuiltInLootTables.NETHER_BRIDGE)
 				.addModifier(new LootPoolEntriesModifier(false, 0, List.of(
 						lootPool(CCItems.SILVER_INGOT.get(), 5, 1, 5),
-						lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 6))))
+						lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 6),
+						lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 5))))
 				.addModifier(new LootPoolEntriesModifier(false, 1, List.of(lootPool(CCItems.EXILE_ARMOR_TRIM_SMITHING_TEMPLATE.get(), 1))));
 		this.entry("bastion_bridge").selects(BuiltInLootTables.BASTION_BRIDGE)
 				.addModifier(new LootPoolsModifier(List.of(LootPool.lootPool().name(CavernsAndChasms.MOD_ID + ":golden_bucket").add(LootItem.lootTableItem(CCItems.GOLDEN_BUCKET.get())).build()), false));
 
 		this.entry("end_city_treasure").selects(BuiltInLootTables.END_CITY_TREASURE).addModifier(new LootPoolEntriesModifier(false, 0,
-				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 2, 7), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1))));
+				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 2, 7), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1), lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 1))));
 
 		this.entry("kousa_sanctum").selects(new ResourceLocation("atmospheric", "chests/kousa_sanctum")).addModifier(new LootPoolEntriesModifier(false, 0,
-				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 2, 7), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1))));
+				List.of(lootPool(CCItems.SILVER_INGOT.get(), 15, 2, 7), lootPool(CCItems.SILVER_HORSE_ARMOR.get(), 1), lootPool(CCItems.COPPER_HORSE_ARMOR.get(), 1))));
 
 		this.entry("copper_ore").selects(new ResourceLocation("blocks/copper_ore")).addModifier(new LootPoolsModifier(
 				List.of(LootPool.lootPool().name(CavernsAndChasms.MOD_ID + ":turqouise").setRolls(ConstantValue.exactly(1.0F))
