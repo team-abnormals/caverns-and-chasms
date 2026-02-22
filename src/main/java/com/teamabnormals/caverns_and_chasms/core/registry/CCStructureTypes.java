c16d430d3e76a0779e6dabf8c2e3c2418c203b08
fivelol <44749261+fivelol@users.noreply.github.com>
2026-02-22 16:43:47 +0000

    small fix

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCStructureTypes.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCStructureTypes.java
@@ -131,7 +131,7 @@ public class CCStructureTypes {
 		public static final List<Entry> FORGES = List.of(of("forge", 4));
 		public static final List<Entry> ROOVES = List.of(of("roof", 2));
 		public static final List<Entry> ENTRANCES = List.of(of("gate", 6), of("broken_gate", 4));
-		public static final List<Entry> FIREPLACES = List.of(of("fireplace", 2));
+		public static final List<Entry> FIREPLACES = List.of(of("fireplace", 3));
 		public static final List<Entry> ARCHAEOLOGY = List.of(of("gravel_pile", 32));
 		public static final List<Entry> DECORATIONS = List.of(of("oak_platform", 8), of("oak_shelf", 2), of("tnt_pile", 3));
 		public static final List<Entry> SMALL_DECORATIONS = List.of(of("empty", 1, 22), of("cauldron", 1), of("furnace", 1), of("blast_furnace", 1), of("damaged_anvil", 1), of("dimmer", 1), of("dimmer_scaffolding", 1), of("anvil", 1), of("tnt", 2), of("tnt_scaffolding", 2), of("water_cauldron", 3), of("scaffolding", 4));
