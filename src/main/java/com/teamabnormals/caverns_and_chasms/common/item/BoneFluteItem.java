975057ea5d7ea87ca8d77ff19bddd4bcaf737db1
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2026-03-02 22:57:47 +0200

    Bone flute use anim

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/item/BoneFluteItem.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/item/BoneFluteItem.java
@@ -1,18 +1,32 @@
 package com.teamabnormals.caverns_and_chasms.common.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.teamabnormals.caverns_and_chasms.common.entity.animal.Rat;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCSoundEvents;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.HumanoidModel.ArmPose;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.joml.Matrix3f;
+import org.joml.Vector3f;
+
+import java.util.function.Consumer;
 
 public class BoneFluteItem extends Item {
 
@@ -38,7 +52,44 @@ public class BoneFluteItem extends Item {
 
 	@Override
 	public UseAnim getUseAnimation(ItemStack stack) {
-		return UseAnim.TOOT_HORN;
+		return UseAnim.CUSTOM;
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+		consumer.accept(new IClientItemExtensions() {
+			private static final HumanoidModel.ArmPose FLUTE_ARM_POSE = HumanoidModel.ArmPose.create("CAVERNS_AND_CHASMS_FLUTE", true, (model, entity, arm) -> {
+				float f = model.head.xRot * 0.8F;
+				float f1 = model.head.yRot * Mth.cos(f);
+				float xRotR = arm == HumanoidArm.RIGHT ? -1.05F : -1.55F;
+				float xRotL = arm == HumanoidArm.RIGHT ? -1.55F : -1.05F;
+				float yRotR = (arm == HumanoidArm.RIGHT ? -0.5F : -0.35F) + f1;
+				float yRotL = (arm == HumanoidArm.RIGHT ? 0.35F : 0.5F) + f1;
+
+				Vector3f vec3R = (new Matrix3f()).rotationZYX(0.0F, yRotR, xRotR).rotateLocalX(f).getEulerAnglesZYX(new Vector3f());
+				Vector3f vec3L = (new Matrix3f()).rotationZYX(0.0F, yRotL, xRotL).rotateLocalX(f).getEulerAnglesZYX(new Vector3f());
+
+				model.rightArm.setRotation(vec3R.x, vec3R.y, vec3R.z);
+				model.leftArm.setRotation(vec3L.x, vec3L.y, vec3L.z);
+			});
+
+			@Override
+			public HumanoidModel.ArmPose getArmPose(LivingEntity living, InteractionHand hand, ItemStack stack) {
+				return living.getUsedItemHand() == hand && living.getUseItemRemainingTicks() > 0 ? FLUTE_ARM_POSE : ArmPose.ITEM;
+			}
+
+			@Override
+			public boolean applyForgeHandTransform(PoseStack poseStack, LocalPlayer player, HumanoidArm arm, ItemStack itemInHand, float partialTick, float equipProcess, float swingProcess) {
+				if (player.isUsingItem() && player.getUseItemRemainingTicks() > 0) {
+					int i = arm == HumanoidArm.RIGHT ? 1 : -1;
+					poseStack.translate((float) i * 0.56F, -0.52F, -0.72F);
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
 	}
 
 	@Override
--- src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/client/ItemInHandRendererMixin.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/client/ItemInHandRendererMixin.java
@@ -2,8 +2,11 @@ package com.teamabnormals.caverns_and_chasms.core.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.teamabnormals.caverns_and_chasms.common.item.MonocleItem;
+import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
 import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.client.renderer.ItemInHandRenderer.HandRenderSelection;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
@@ -11,6 +14,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemInHandRenderer.class)
 public class ItemInHandRendererMixin {
@@ -19,6 +23,13 @@ public class ItemInHandRendererMixin {
 	private void isScoping(AbstractClientPlayer player, float p_109373_, float p_109374_, InteractionHand p_109375_, float p_109376_, ItemStack p_109377_, float p_109378_, PoseStack p_109379_, MultiBufferSource p_109380_, int p_109381_, CallbackInfo ci) {
 		if (MonocleItem.isScopingMonocle(player)) {
 			ci.cancel();
+		}
+	}
+
+	@Inject(at = @At(value = "RETURN", ordinal = 0), method = "evaluateWhichHandsToRender", cancellable = true)
+	private static void evaluateWhichHandsToRender(LocalPlayer player, CallbackInfoReturnable<HandRenderSelection> cir) {
+		if (player.getUseItem().is(CCItems.BONE_FLUTE.get())) {
+			cir.setReturnValue(HandRenderSelection.onlyForHand(player.getUsedItemHand()));
 		}
 	}
 }
