fd862f4da662e8765bbd6f516c6a3e4c56faa9c7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-03-12 21:32:52 -0500

    Update CCEvents.java

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCEvents.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/other/CCEvents.java
@@ -750,7 +750,7 @@ public class CCEvents {
 					projectile.checkInsideBlocks();
 
 					SoundType soundType = state.getBlock().getSoundType(state, level, pos, null);
-					SoundEvent soundEvent = soundType instanceof TinSoundType tinSoundType ? tinSoundType.getDeflectSound() : ricochetArrow ? CCSoundEvents.GRAZER_RICOCHET.get() : CCSoundEvents.TIN_DEFLECT.get();
+					SoundEvent soundEvent = soundType instanceof TinSoundType tinSoundType ? tinSoundType.getDeflectSound() : ricochetArrow ? CCSoundEvents.RICOCHET_ARROW_DEFLECT.get() : CCSoundEvents.TIN_DEFLECT.get();
 					playRicochetEffects(level, location, movement.reverse().normalize(), speed, soundEvent, soundType == CCSoundTypes.STORAGE_DUCT ? 0.5F : 1.0F, random, false);
 
 					event.setCanceled(true);
