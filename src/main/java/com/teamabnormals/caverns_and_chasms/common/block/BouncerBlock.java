f281220f66a38d5421c1552c3c792bc9a429b7f6
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-03-14 08:28:52 -0500

    Update BouncerBlock.java

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/block/BouncerBlock.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/block/BouncerBlock.java
@@ -1,7 +1,11 @@
 package com.teamabnormals.caverns_and_chasms.common.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.TargetBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class BouncerBlock extends TargetBlock {
 
@@ -12,5 +16,10 @@ public class BouncerBlock extends TargetBlock {
 	@Override
 	public float getJumpFactor() {
 		return 1.75F;
+	}
+
+	@Override
+	public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float distance) {
+		entity.causeFallDamage(distance * (4.0F / 7.0F), 1.0F, level.damageSources().fall());
 	}
 }
