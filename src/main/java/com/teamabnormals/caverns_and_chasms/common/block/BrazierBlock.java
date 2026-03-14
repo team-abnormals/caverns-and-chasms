6372cf8040681862d57d13533e071ee7a613ea89
fivelol <44749261+fivelol@users.noreply.github.com>
2026-03-14 02:14:40 +0000

    hopefully make these work

--- src/main/java/com/teamabnormals/caverns_and_chasms/common/block/BrazierBlock.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/common/block/BrazierBlock.java
@@ -137,7 +137,7 @@ public class BrazierBlock extends Block implements SimpleWaterloggedBlock {
 
 	@Override
 	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-		if (state.getValue(LIT) && random.nextInt(10) == 0) {
+		if (state.getValue(LIT) && random.nextInt(2) == 0) {
 			level.playLocalSound((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, CCSoundEvents.BRAZIER_CRACKLE.get(), SoundSource.BLOCKS, 1.0F + random.nextFloat(), random.nextFloat() * 0.7F + 0.6F, false);
 		}
 	}
