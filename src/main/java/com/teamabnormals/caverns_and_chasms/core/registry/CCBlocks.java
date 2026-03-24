aa5ae9c9dead42df6fc5eb3740c281cb78edc31a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-03-24 15:53:19 -0500

    Remove unused class and add a tag

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCBlocks.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCBlocks.java
@@ -106,7 +106,7 @@ public class CCBlocks {
 	public static final RegistryObject<Block> HOOP = HELPER.createBlock("hoop", () -> new HoopBlock(CCProperties.HOOP));
 	public static final RegistryObject<Block> STORAGE_DUCT = HELPER.createBlock("storage_duct", () -> new StorageDuctBlock(CCProperties.STORAGE_DUCT));
 	public static final RegistryObject<Block> STORAGE_DUCT_HATCH = HELPER.createBlock("storage_duct_hatch", () -> new StorageDuctHatchBlock(CCProperties.STORAGE_DUCT_HATCH));
-	public static final RegistryObject<Block> TINPLATE_BLOCK = HELPER.createBlock("tinplate_block", () -> new TinplateBlock(BlockBehaviour.Properties.copy(TIN_BLOCK.get()).sound(CCSoundTypes.TINPLATE)));
+	public static final RegistryObject<Block> TINPLATE_BLOCK = HELPER.createBlock("tinplate_block", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(TIN_BLOCK.get()).sound(CCSoundTypes.TINPLATE)));
 
 	public static final RegistryObject<Block> ROLLER_DOOR = HELPER.createBlockNoItem("roller_door", () -> new RollerDoorBlock(false, MovingDoorType.ROLLER_DOOR, CCProperties.ROLLER_DOOR));
 	public static final RegistryObject<Block> ROLLER_DOOR_HEADER = HELPER.createBlockNoItem("roller_door_header", () -> new RollerDoorBlock(true, MovingDoorType.ROLLER_DOOR, CCProperties.ROLLER_DOOR));
