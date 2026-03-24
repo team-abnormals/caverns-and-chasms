8db8a3f8b62943d7af371fe2d099762ecddd068f
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2026-03-24 23:14:37 +0200

    Made tin spark particles smoother

--- src/main/java/com/teamabnormals/caverns_and_chasms/client/particle/TinSparkParticle.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/client/particle/TinSparkParticle.java
@@ -12,6 +12,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import org.joml.Vector3f;
 
 public class TinSparkParticle extends SimpleAnimatedParticle {
+	private Vec3 prevDisplacement;
+	private Vec3 displacement;
+
 	protected TinSparkParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, SpriteSet spriteSet) {
 		super(level, x, y, z, spriteSet, 0.1F);
 		this.xd = xSpeed;
@@ -23,14 +26,21 @@ public class TinSparkParticle extends SimpleAnimatedParticle {
 		this.gravity = 0.75F;
 		this.setSize(0.02F, 0.02F);
 		this.quadSize *= this.random.nextFloat() * 0.6F + 0.5F;
+
+		this.displacement = new Vec3(this.x + this.xd, this.y + this.yd, this.z + this.zd);
+		this.prevDisplacement = this.displacement;
 	}
 
 	public void tick() {
+		this.prevDisplacement = this.displacement;
+
 		super.tick();
 		float f = (float) this.age / this.lifetime;
 		this.gCol = 1.0F - f * 0.5F;
 		this.bCol = 1.0F - f;
 		this.alpha = 1.0F - f * 0.5F;
+
+		this.displacement = new Vec3(this.x - this.xo, this.y - this.yo, this.z - this.zo);
 	}
 
 	@Override
@@ -59,19 +69,19 @@ public class TinSparkParticle extends SimpleAnimatedParticle {
 	}
 
 	@Override
-	public void render(VertexConsumer vertexConsumer, Camera camera, float partialTicks) {
+	public void render(VertexConsumer vertexConsumer, Camera camera, float partialTick) {
 		Vec3 vec3 = camera.getPosition();
-		float x = (float) (Mth.lerp(partialTicks, this.xo, this.x) - vec3.x());
-		float y = (float) (Mth.lerp(partialTicks, this.yo, this.y) - vec3.y());
-		float z = (float) (Mth.lerp(partialTicks, this.zo, this.z) - vec3.z());
+		float x = (float) (Mth.lerp(partialTick, this.xo, this.x) - vec3.x());
+		float y = (float) (Mth.lerp(partialTick, this.yo, this.y) - vec3.y());
+		float z = (float) (Mth.lerp(partialTick, this.zo, this.z) - vec3.z());
 
-		Vec3 velocity = new Vec3(this.x - this.xo, this.y - this.yo, this.z - this.zo);
+		Vec3 displacement = this.prevDisplacement.lerp(this.displacement, partialTick);
 
-		Vec3 forward = velocity.lengthSqr() > 0.0001D ? velocity.normalize() : new Vec3(0.0D, -1.0D, 0.0D);
-		Vec3 towardscamera = new Vec3(-x, -y, -z);
-		Vec3 side = towardscamera.subtract(forward.scale(towardscamera.dot(forward) / forward.dot(forward))).normalize().cross(forward);
+		Vec3 forward = displacement.lengthSqr() > 0.0001D ? displacement.normalize() : new Vec3(0.0D, -1.0D, 0.0D);
+		Vec3 towardsCamera = new Vec3(-x, -y, -z);
+		Vec3 side = towardsCamera.subtract(forward.scale(towardsCamera.dot(forward) / forward.dot(forward))).normalize().cross(forward);
 
-		float f3 = this.getQuadSize(partialTicks);
+		float f3 = this.getQuadSize(partialTick);
 		Vector3f[] avector3f = new Vector3f[]{
 				side.reverse().subtract(forward).toVector3f().mul(f3).add(x, y, z),
 				side.reverse().add(forward).toVector3f().mul(f3).add(x, y, z),
@@ -83,7 +93,7 @@ public class TinSparkParticle extends SimpleAnimatedParticle {
 		float f7 = this.getU1();
 		float f4 = this.getV0();
 		float f5 = this.getV1();
-		int light = this.getLightColor(partialTicks);
+		int light = this.getLightColor(partialTick);
 
 		vertexConsumer.vertex(avector3f[0].x(), avector3f[0].y(), avector3f[0].z()).uv(f7, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(light).endVertex();
 		vertexConsumer.vertex(avector3f[1].x(), avector3f[1].y(), avector3f[1].z()).uv(f7, f4).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(light).endVertex();
