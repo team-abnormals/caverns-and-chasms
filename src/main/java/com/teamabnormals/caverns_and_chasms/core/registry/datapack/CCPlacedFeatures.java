baffc59e634a45c80562785b2fba1915a1d6c908
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-03-26 00:15:18 -0500

    Add additional depth range to tin arrows a bit

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/datapack/CCPlacedFeatures.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/datapack/CCPlacedFeatures.java
@@ -61,8 +61,8 @@ public class CCPlacedFeatures {
 		register(context, ORE_SILVER_EXTRA, CCConfiguredFeatures.ORE_SILVER, commonOrePlacement(50, HeightRangePlacement.uniform(VerticalAnchor.absolute(32), VerticalAnchor.absolute(256))));
 		register(context, ORE_SILVER_SOUL, CCConfiguredFeatures.ORE_SOUL_SILVER, commonOrePlacement(45, PlacementUtils.RANGE_10_10));
 
-		register(context, ORE_TIN, CCConfiguredFeatures.ORE_TIN, orePlacement(TinArrowPlacement.of(1, 256), HeightRangePlacement.triangle(VerticalAnchor.absolute(0), VerticalAnchor.absolute(48))));
-		register(context, ORE_TIN_BURIED, CCConfiguredFeatures.ORE_TIN_BURIED, orePlacement(TinArrowPlacement.of(1, 320), HeightRangePlacement.triangle(VerticalAnchor.absolute(16), VerticalAnchor.absolute(32))));
+		register(context, ORE_TIN, CCConfiguredFeatures.ORE_TIN, orePlacement(TinArrowPlacement.of(1, 256), HeightRangePlacement.triangle(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(48))));
+		register(context, ORE_TIN_BURIED, CCConfiguredFeatures.ORE_TIN_BURIED, orePlacement(TinArrowPlacement.of(1, 320), HeightRangePlacement.triangle(VerticalAnchor.absolute(0), VerticalAnchor.absolute(32))));
 
 		register(context, ORE_SPINEL, CCConfiguredFeatures.ORE_SPINEL, commonOrePlacement(2, HeightRangePlacement.triangle(VerticalAnchor.absolute(-32), VerticalAnchor.absolute(64))));
 		register(context, ORE_SPINEL_BURIED, CCConfiguredFeatures.ORE_SPINEL_BURIED, commonOrePlacement(4, HeightRangePlacement.triangle(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(48))));
