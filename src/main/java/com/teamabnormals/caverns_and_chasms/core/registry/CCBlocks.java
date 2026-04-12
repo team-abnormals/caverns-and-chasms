5246f3867d75da95e6c4d2da174c48b03286c440
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-04-11 23:58:36 -0500

    Fixes #213

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCBlocks.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/registry/CCBlocks.java
@@ -154,7 +154,7 @@ public class CCBlocks {
 
 	public static final DeferredBlock<Block> ROTTEN_FLESH_BLOCK = HELPER.createBlock("rotten_flesh_block", () -> new Block(CCProperties.ROTTEN_FLESH_BLOCK));
 
-	public static final DeferredBlock<Block> GUNPOWDER_BLOCK = HELPER.createBlock("gunpowder_block", () -> new GunpowderBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GRAY).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sound(CCSoundTypes.GUNPOWDER)));
+	public static final DeferredBlock<Block> GUNPOWDER_BLOCK = HELPER.createBlock("gunpowder_block", () -> new GunpowderBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GRAY).instrument(NoteBlockInstrument.SNARE).strength(0.5F, 0.0F).sound(CCSoundTypes.GUNPOWDER)));
 
 	public static final Pair<DeferredBlock<SparklerBlock>, DeferredBlock<WallSparklerBlock>> SPARKLER = HELPER.createSparklerBlock("sparkler", "wall_sparkler", CCParticleTypes.SPARKLER_SPARK);
 	public static final Pair<DeferredBlock<SparklerBlock>, DeferredBlock<WallSparklerBlock>> WHITE_SPARKLER = HELPER.createSparklerBlock("white_sparkler", "white_wall_sparkler", CCParticleTypes.WHITE_SPARKLER_SPARK);
