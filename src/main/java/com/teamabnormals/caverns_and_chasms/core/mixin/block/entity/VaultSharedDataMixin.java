0dd406dc8961a979114c5dbb516c4233f635ebc4
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-04-14 16:52:33 -0500

    Ominous Vaults won't Activate when holding a non-ominous token and vice versa

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/block/entity/VaultSharedDataMixin.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/mixin/block/entity/VaultSharedDataMixin.java
@@ -1,12 +1,18 @@
 package com.teamabnormals.caverns_and_chasms.core.mixin.block.entity;
 
+import com.teamabnormals.caverns_and_chasms.core.other.CCDataMaps;
+import com.teamabnormals.caverns_and_chasms.core.other.CCDataMaps.TrialToken;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.vault.VaultBlockEntity;
 import net.minecraft.world.level.block.entity.vault.VaultConfig;
 import net.minecraft.world.level.block.entity.vault.VaultServerData;
 import net.minecraft.world.level.block.entity.vault.VaultSharedData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -29,12 +35,20 @@ public abstract class VaultSharedDataMixin {
 		Set<UUID> set = config.playerDetector()
 				.detect(level, config.entitySelector(), pos, deactivationRange, false)
 				.stream()
-				.filter(player -> !((VaultServerDataAccessor) serverData).invokeHasRewardedPlayer(level.getPlayerByUUID(player)))
+				.filter(uuid -> !serverData.getRewardedPlayers().contains(uuid) || canUseTokenOnVault(level, config, uuid))
 				.collect(Collectors.toSet());
 		if (!this.connectedPlayers.equals(set)) {
 			this.connectedPlayers = set;
 			this.markDirty();
 		}
 		ci.cancel();
+	}
+
+	@Unique
+	private static boolean canUseTokenOnVault(ServerLevel level, VaultConfig config, UUID uuid) {
+		Player player = level.getPlayerByUUID(uuid);
+		ItemStack stack = player.getMainHandItem();
+		TrialToken token = stack.getItemHolder().getData(CCDataMaps.TRIAL_TOKENS);
+		return token != null && VaultBlockEntity.Server.isValidToInsert(config, stack);
 	}
 }
