d3881bdc75f033e42ff29093be6fee98b150bd0a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-04-16 08:11:11 -0500

    Update AtoningTableEnchantmentNames.java

--- src/main/java/com/teamabnormals/caverns_and_chasms/client/gui/screens/inventory/AtoningTableEnchantmentNames.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/client/gui/screens/inventory/AtoningTableEnchantmentNames.java
@@ -12,7 +12,6 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.apache.commons.lang3.ArrayUtils;
 
-@OnlyIn(Dist.CLIENT)
 public class AtoningTableEnchantmentNames {
 	private static final ResourceLocation KOUKERI_FONT = CavernsAndChasms.location("koukeri");
 	private static final Style ROOT_STYLE = Style.EMPTY.withFont(KOUKERI_FONT);
