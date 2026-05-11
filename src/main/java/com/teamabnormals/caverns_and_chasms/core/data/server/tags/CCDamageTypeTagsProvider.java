1874ae080ad0174b3e2a4688772c1c1253125b83
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-05-11 09:44:23 -0500

    Fixed some damage sources providing knockback; Closes #240

--- src/main/java/com/teamabnormals/caverns_and_chasms/core/data/server/tags/CCDamageTypeTagsProvider.java
+++ src/main/java/com/teamabnormals/caverns_and_chasms/core/data/server/tags/CCDamageTypeTagsProvider.java
@@ -5,6 +5,7 @@ import com.teamabnormals.caverns_and_chasms.core.other.tags.CCDamageTypeTags;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.DamageTypeTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageType;
@@ -14,10 +15,10 @@ import java.util.concurrent.CompletableFuture;
 
 import static com.teamabnormals.caverns_and_chasms.core.registry.datapack.CCDamageTypes.*;
 
-public class CCDamageTypeTagsProvider extends TagsProvider<DamageType> {
+public class CCDamageTypeTagsProvider extends DamageTypeTagsProvider {
 
 	public CCDamageTypeTagsProvider(PackOutput output, CompletableFuture<Provider> provider, ExistingFileHelper helper) {
-		super(output, Registries.DAMAGE_TYPE, provider, CavernsAndChasms.MOD_ID, helper);
+		super(output, provider, CavernsAndChasms.MOD_ID, helper);
 	}
 
 	@Override
@@ -27,6 +28,7 @@ public class CCDamageTypeTagsProvider extends TagsProvider<DamageType> {
 		this.tag(DamageTypeTags.IS_FIRE).add(LAVA_LAMP);
 		this.tag(DamageTypeTags.IS_PROJECTILE).add(KUNAI);
 		this.tag(DamageTypeTags.NO_ANGER).add(GRAZER);
+		this.tag(DamageTypeTags.NO_KNOCKBACK).add(LAVA_LAMP, SPIKED_RAIL, DRAINING);
 		this.tag(CCDamageTypeTags.BYPASSES_TETHER_POTIONS);
 		this.tag(CCDamageTypeTags.DRAINS_ENEMIES).add(DRAINING);
 	}
